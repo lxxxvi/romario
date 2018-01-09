432ad155a3b078e956e79b4ea1b6ea379799cee2
Mario Schuettel <github@lxxxvi.ch>
2018-01-09 16:09:25 +0100

    cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,10 @@
 this_directory_path  = File.expand_path(File.dirname(__FILE__))
 sql_seeds_directory_path = Pathname.new("#{this_directory_path}/seeds/sql/")
 
-
 db_connection = Hanami::Model.configuration.connection
-# [
-#   'groups.rb',
-#   'teams.rb'
-# ].each do |seeds_file_name|
-#   seeds_file_path = "#{seeds_directory_path}/#{seeds_file_name}"
-#   load seeds_file_path
-# end
+
 sql_seeds_directory_path.children.sort.each do |sql_file|
   puts "loading #{sql_file}"
   db_connection.execute(sql_file.read)
 end
-
-# Hanami::Model.configuration.url
-#  => "postgresql://localhost/romario_development"
-
 
--- db/seeds/groups.rb
+++
@@ -1,5 +0,0 @@
-group_repository = GroupRepository.new
-
-("A".."H").each do |letter|
-  group_repository.find_or_create_by_letter!(letter)
-end
