53a95331e6f638d77ac279e6bdd1f6201f820b6b
Mario Schuettel <github@lxxxvi.ch>
2018-01-09 16:40:42 +0100

    Add users and predictions table

---
+++ db/migrations/20180109150006_create_predictions.rb
@@ -0,0 +1,22 @@
+Hanami::Model.migration do
+  change do
+    create_table :predictions do
+      primary_key :id
+
+      foreign_key :user_id      , :users      , on_delete: :cascade, null: false
+      foreign_key :match_id     , :matches    , on_delete: :cascade, null: false
+
+      column :predicted_left_team_score       , Integer, null: true
+      column :predicted_right_team_score      , Integer, null: true
+
+      column :points_left_team_score          , Integer, null: true
+      column :points_right_team_score         , Integer, null: true
+      column :points_overall_outcome          , Integer, null: true
+      column :points_goal_difference          , Integer, null: true
+    end
+
+    alter_table :predictions do
+      add_unique_constraint [:user_id, :match_id]
+    end
+  end
+end
