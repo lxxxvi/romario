cd685cc3096ec8b971390cbab9b09ccbd3d083b9
Cedric Wider <cedric.wider@simplificator.com>
2018-01-11 13:23:49 +0100

    first running spec!!!

--- spec/romario/repositories/match_repository_spec.rb
+++ spec/romario/repositories/match_repository_spec.rb
@@ -5,12 +5,14 @@ require_relative '../../spec_helper'
 describe MatchRepository do
   subject { described_class.new }
 
+  let(:user) { UserRepository.new.first }
+
   before do
     # TODO: create test data
   end
 
   it 'fetches all matches with predictions' do
-    user_predictions = repository.with_predictions_of_user(user)
-    expect(user_predictions.count).to eq 1
+    user_predictions = subject.with_predictions_of_user(user)
+    expect(user_predictions.count).to eq 44
   end
 end
